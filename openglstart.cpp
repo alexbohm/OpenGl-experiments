cd2e9ff5f26f83f54981f375cd14b755f6622d26
Alex Bohm <aabohm7@gmail.com>
2017-02-24 17:28:25 -0600

    Add files via upload

--- openglstart.cpp
+++ openglstart.cpp
@@ -10,33 +10,33 @@ using namespace std;
 double rotation;
 static void display()
 {
-	glClear(GL_COLOR_BUFFER_BIT);
+	glClear (GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	glBegin(GL_TRIANGLES);
-		glColor3f(0.1,0.1,0.1);
+		glColor3f(0.01f,0.01f,0.01f);
 		glVertex3f(0,0,0);
 		glColor3f(1,1,1);
 		glVertex3f(1,0,0);
 		glColor3f(1,1,1);
 		glVertex3f(0,0,1);
 
-		glColor3f(0.1,0.1,0.1);
+		glColor3f(0.01f,0.01f,0.01f);
 		glVertex3f(0,0,0);
 		glColor3f(1,1,1);
 		glVertex3f(0,1,0);
 		glColor3f(1,1,1);
 		glVertex3f(0,0,1);
 
-		glColor3f(0.1,0.1,0.1);
+		glColor3f(0.01f,0.01f,0.01f);
 		glVertex3f(0,0,0);
 		glColor3f(1,1,1);
 		glVertex3f(0,1,0);
 		glColor3f(1,1,1);
 		glVertex3f(1,0,0);
 	glEnd();
+	
 	glLoadIdentity();
 	glRotated(45,1,0,0);
 	glRotated(rotation,0,1,0);
-
 	glutSwapBuffers();
 };
 void mouseClick(int button, int state, int x, int y){
@@ -51,18 +51,20 @@ void mouseClick(int button, int state, int x, int y){
 }
 int main(int argc, char** argv)
 {
+	glEnable(GL_DEPTH_TEST);
+	glDepthFunc(GL_GREATER);
+
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_DOUBLE|GLUT_RGBA);
 	glutInitWindowSize(1000, 1000);
 	glutInitWindowPosition(100, 100);
-	glEnable(GL_DEPTH_TEST);
-	glDepthFunc(GL_LEQUAL);
 	glutCreateWindow("Test");
-
-	glutDisplayFunc(display);
-
-	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+	
 	glutMouseFunc(mouseClick);
+	glutDisplayFunc(display);
+	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+	//glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+	
 	
 	glutMainLoop();
 	
